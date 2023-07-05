067af25db5e19fb2a77545e6bf52fa53413f4d53
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-07-05 22:11:39 +0200

    update script

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const countryList = document.getElementById('country-list');  //hacer referencia al HTML
-function getCountry() { 
-  fetch('https://restcountries.com/v3.1/all')
-    .then(response => response.json())
-    .then(paises => {
-      console.log(paises);
+function getCountry() { //funcion para obtener los paises
+  fetch('https://restcountries.com/v3.1/all')//hacer la peticion
+    .then(response => response.json())//convertir la respuesta a json
+    .then(paises => {//recorrer el json
+      console.log(paises);//imprimir en consola
 
-      paises.forEach(pais => {
-        const list = document.createElement('li');
-        list.textContent = pais.name.common;
+      paises.forEach(pais => {//recorrer el json
+        const list = document.createElement('li'); //crear un elemento li
+        list.textContent = pais.name.common;//agregar el nombre del pais
 
-        const flagImg = document.createElement('img');
-        flagImg.src = pais.flags.png;
+        const flagImg = document.createElement('img');//crear un elemento img
+        flagImg.src = pais.flags.png;//agregar la imagen del pais
 
-        list.appendChild(flagImg);
-        countryList.appendChild(list);
+        list.appendChild(flagImg);//agregar la imagen al li
+        countryList.appendChild(list);//agregar el li al ul
       });
     })
-    .catch(error => console.error(error));
+    .catch(error => console.error(error));//imprimir el error en consola
 }
 
-getCountry();  
+getCountry();  //llamar a la funcion
